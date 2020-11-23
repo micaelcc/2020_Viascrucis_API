7b155b051d571aad043a3cea9723a6948f0616ca
micaelcc <50875854+micaelcc@users.noreply.github.com>
2020-11-22 21:56:45 -0300

    Update admin-stories-routes.js

--- src/routes/admin-stories-routes.js
+++ src/routes/admin-stories-routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const adminStoriesController = require('../controllers/admin-stories-controller');
 
 router.get('/', adminStoriesController.listStories);
+router.put('/', adminStoriesController.approveStory);
 router.delete('/', adminStoriesController.deleteStory);
+
 
 module.exports = router;
