c4bcd289038771b4f6338761693536c287a8be10
micaelcc <micaelcomp@alu.ufc.br>
2020-11-28 16:17:29 -0300

    jwt auth

--- src/config/login.js
+++ src/config/login.js
@@ -2,8 +2,9 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
     try{
-        const decode = jwt.verify(req.body.token, process.env.SECRET);
-        req.usuario = decode
+        const token = req.headers.authorization.split(' ')[1];
+        const decode = jwt.verify(token, process.env.SECRET);
+        req.usuario = decode;
         next()
     }catch(e){
         return res.status(401).send({message: 'Falha na autorização'})
