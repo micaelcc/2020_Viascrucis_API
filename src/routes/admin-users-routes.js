01376c9680cdd07a00c91875e3b98eaeb6a6ef4d
Micael Lima <50875854+micaelcc@users.noreply.github.com>
2020-11-29 10:26:50 -0800

    Update admin-users-routes.js

--- src/routes/admin-users-routes.js
+++ src/routes/admin-users-routes.js
@@ -4,7 +4,6 @@ const adminController = require('../controllers/admin-users-controller');
 const login = require('../config/login')
 
 router.post('/login', adminController.login);
-router.get('/list', login, adminController.listAdmins);
 router.post('/create', login,  adminController.createAdminUser);
 
 module.exports = router;
